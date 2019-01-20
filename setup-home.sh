71efb71d7d831de220e1e59fc7803a3c3115342e
Timóteo Ramos <timoteoramos@ymail.com>
2019-01-20 19:10:44 -0300

    GTK themes

--- setup-home.sh
+++ setup-home.sh
@@ -3,12 +3,16 @@ SCRIPT_ROOT=$(cd `dirname "$0"` && pwd)
 
 # Setup directory structure
 mkdir -p ~/.vim/bundle
-mkdir -p ~/.config/{qtile,rofi}
+mkdir -p ~/.config/{gtk-3.0,qtile,rofi}
+mkdir -p ~/Imagens/screenshots
 
 # Vundle
 [ ! -e ~/.vim/bundle/Vundle.vim ] && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Symbolic links
+rm -Rf ~/.config/gtk-3.0/*
+ln -s $SCRIPT_ROOT/gtk/settings.ini ~/.config/gtk-3.0/
+
 rm -Rf ~/.config/qtile/*
 ln -s $SCRIPT_ROOT/qtile/{autostart,config,groups,keys,layouts,misc,screens}.py ~/.config/qtile/
 ln -s $SCRIPT_ROOT/qtile/autostart.sh ~/.config/qtile/
@@ -18,6 +22,9 @@ ln -s $SCRIPT_ROOT/rofi/config ~/.config/rofi/
 
 rm -Rf ~/.bashrc
 ln -s $SCRIPT_ROOT/bash/bashrc ~/.bashrc
+
+rm -Rf ~/.gtkrc-2.0
+ln -s $SCRIPT_ROOT/gtk/gtkrc-2.0 ~/.gtkrc-2.0
 
 rm -Rf ~/.tmux.conf
 ln -s $SCRIPT_ROOT/tmux/tmux.conf ~/.tmux.conf
--- setup-void.sh
+++ setup-void.sh
@@ -4,8 +4,11 @@ echo "Setting up root configuration and essential packages"
 mkdir -p /etc/X11/xorg.conf.d
 cp rootconfig/20-kbdlayout.conf /etc/X11/xorg.conf.d/
 cp rootconfig/environment /etc/environment
-xbps-install -S xorg-minimal xorg-fonts xrandr qt5-styleplugins adwaita-icon-theme gnome-icon-theme gnome-icon-theme-extras gnome-icon-theme-symbolic ConsoleKit2 mate-polkit curl base-devel vim-huge-python3 python3-Cython python3-devel python3-pip editorconfig
+xbps-install -S xorg-minimal xorg-fonts xrandr qt5-styleplugins adwaita-icon-theme gnome-icon-theme gnome-icon-theme-extras gnome-icon-theme-symbolic ConsoleKit2 mate-polkit curl wget vim-huge-python3
 [ ! -L /var/service/dbus ] && ln -s /etc/sv/dbus /var/service/
+
+echo "Development tools"
+xbps-install -S git subversion bzr mercurial cvs base-devel python3-Cython python3-devel python3-pip editorconfig
 
 echo "Powerline setup"
 xbps-install -S font-hack-ttf socat python3-psutil python3-GitPython
@@ -16,3 +19,9 @@ xbps-install -S libxcb-devel libffi-devel cairo-devel python3-dbus python3-xdg p
 pip3 install xcffib
 pip3 install cairocffi
 pip3 install qtile
+
+echo "GTK themes"
+xbps-install -S greybird-themes papirus-icon-theme
+
+echo "GStreamer plugins"
+xbps-install -S gst-libav gst-omx gst-plugins-bad1 gst-plugins-good1 gst-plugins-ugly1 gstreamer-vaapi
